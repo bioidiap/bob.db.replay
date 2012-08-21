bf9114ed2b0a8f90e536002fc722e0b5f0f16321
Andre Anjos <andre.dos.anjos@gmail.com>
2012-08-21 23:29:45 +0200

    Add classifiers

--- setup.py
+++ setup.py
@@ -53,4 +53,13 @@ setup(
         ],
       },
 
+    classifiers = [
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Natural Language :: English',
+      'Programming Language :: Python',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      'Topic :: Database :: Front-Ends',
+      ],
 )
