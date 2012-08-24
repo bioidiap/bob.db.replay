851fa906ecc3ef4f32ebf8b9912b7680aebe901a
Andre Anjos <andre.anjos@idiap.ch>
2012-08-24 16:48:12 +0200

    Set version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.replay',
-    version='master',
+    version='1.0.0a1',
     description='Replay Attack Database Access API for Bob',
     url='http://github.com/bioidiap/bob.db.replay',
     license='GPLv3',
