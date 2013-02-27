4a550a66eeb0636636ae9ba3fd4f97fe06c63b9a
Andre Anjos <andre.anjos@idiap.ch>
2013-02-27 13:04:11 +0100

    Bump-up setup version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.replay',
-    version='1.0.1',
+    version='1.0.2',
     description='Replay Attack Database Access API for Bob',
     url='http://pypi.python.org/pypi/xbob.db.replay',
     license='GPLv3',
