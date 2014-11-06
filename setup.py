1e7663688b1b77b124f457c5266acedf2c879097
Manuel Guenther <manuel.guenther@idiap.ch>
2014-11-06 15:36:08 +0100

    Implemented versioning by version.txt

--- setup.py
+++ setup.py
@@ -5,12 +5,15 @@
 
 from setuptools import setup, find_packages
 
+# Define package version
+version = open("version.txt").read().rstrip()
+
 # The only thing we do in this file is to call the setup() function with all
 # parameters that define our package.
 setup(
 
     name='bob.db.replay',
-    version='2.0.0a0',
+    version=version,
     description='Replay Attack Database Access API for Bob',
     url='http://pypi.python.org/pypi/bob.db.replay',
     license='GPLv3',
